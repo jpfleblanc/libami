b09498be22882d078a00fbea0a281a11f2126fc2
Hossam Elazab <hmselazab@mun.ca>
2020-07-30 13:11:53 -0230

    Added new test suite for function "pole_equiv" TODO: fix the way googletest is implemented to CMakeLists

---
+++ test/pole_equiv.cpp
@@ -0,0 +1,163 @@
+
+#include "../src/ami.hpp"
+#include <iomanip>
+#include <random>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <chrono>
+#include <random>
+
+#include "gtest.h"
+// include other headers if necessary   #include ../src/ami.hpps
+// You are going to create two pole_struct (similar to g_struct) then,
+// test both epsilons have the same size, and both have the same alpha
+// size. Then, check each entry in eps and alpha if they are the same
+// for both poles.
+
+
+// Checks if both alpha vectors are equal.
+TEST(pole_equiv, alpha_size){
+	// Create two alpha objects
+	AmiCalc::alpha_t alpha_1={0,0,0,1,0};
+	AmiCalc::alpha_t alpha_2={0,0,0,1,0,0};
+	
+	// Create two epsilon objects
+	AmiCalc::epsilon_t epsilon_1={0,0,0,0,0,1,0};
+	AmiCalc::epsilon_t epsilon_2={0,0,0,0,0,1,0};
+	
+	// Create the pole_struct objects
+	AmiCalc::pole_struct pole_test_1;
+	AmiCalc::pole_struct pole_test_2;
+	
+	// Attach the eps and alpha to the pole_struct
+	pole_test_1.alpha_ = alpha_1;
+	pole_test_1.eps_ = epsilon_1;
+	
+	pole_test_2.alpha_ = alpha_2;
+	pole_test_2.eps_ = epsilon_2;
+	
+	
+	// Testing part
+	AmiCalc obj;
+	ASSERT_FALSE(obj.pole_equiv(pole_test_1, pole_test_2)) << "Alpha size of poles are different" ;
+
+// Test must tell the user where is the mistake (either the size
+// not the same or which entry in the array was different)
+}
+
+
+// Checks if both epsilon vectors are equal
+TEST(pole_equiv, epsilon_size){
+	// Create two alpha objects
+	AmiCalc::alpha_t alpha_1={0,0,0,1,0};
+	AmiCalc::alpha_t alpha_2={0,0,0,1,0};
+	
+	// Create two epsilon objects
+	AmiCalc::epsilon_t epsilon_1={0,0,0,0,1,0};
+	AmiCalc::epsilon_t epsilon_2={0,0,0,0,0,1,0};
+	
+	// Create the pole_struct objects
+	AmiCalc::pole_struct pole_test_1;
+	AmiCalc::pole_struct pole_test_2;
+	
+	// Attach the eps and alpha to the pole_struct
+	pole_test_1.alpha_ = alpha_1;
+	pole_test_1.eps_ = epsilon_1;
+	
+	pole_test_2.alpha_ = alpha_2;
+	pole_test_2.eps_ = epsilon_2;
+	
+	
+	// Testing part
+	AmiCalc obj;
+	ASSERT_FALSE(obj.pole_equiv(pole_test_1, pole_test_2)) << "Epsilon size of poles are different" ;
+}
+
+
+//Checks that elements in alpha vector are not the same
+TEST(pole_equiv, alpha_vector_diff){
+	// Create two alpha objects
+	AmiCalc::alpha_t alpha_1={0,0,1,1,0};
+	AmiCalc::alpha_t alpha_2={0,0,0,1,0};
+	
+	// Create two epsilon objects
+	AmiCalc::epsilon_t epsilon_1={0,0,0,0,0,1,0};
+	AmiCalc::epsilon_t epsilon_2={0,0,0,0,0,1,0};
+	
+	// Create the pole_struct objects
+	AmiCalc::pole_struct pole_test_1;
+	AmiCalc::pole_struct pole_test_2;
+	
+	// Attach the eps and alpha to the pole_struct
+	pole_test_1.alpha_ = alpha_1;
+	pole_test_1.eps_ = epsilon_1;
+	
+	pole_test_2.alpha_ = alpha_2;
+	pole_test_2.eps_ = epsilon_2;
+	
+	
+	// Testing part
+	AmiCalc obj;
+	ASSERT_FALSE(obj.pole_equiv(pole_test_1, pole_test_2)) << "Vector alpha entries are different for both poles." ;
+	
+}	
+
+
+//Checks that elements in epsilon vector are not the same
+TEST(pole_equiv, alpha_vector_diff){
+	// Create two alpha objects
+	AmiCalc::alpha_t alpha_1={0,0,0,1,0};
+	AmiCalc::alpha_t alpha_2={0,0,0,1,0};
+	
+	// Create two epsilon objects
+	AmiCalc::epsilon_t epsilon_1={0,0,2,0,0,1,0};
+	AmiCalc::epsilon_t epsilon_2={0,0,0,0,0,1,0};
+	
+	// Create the pole_struct objects
+	AmiCalc::pole_struct pole_test_1;
+	AmiCalc::pole_struct pole_test_2;
+	
+	// Attach the eps and alpha to the pole_struct
+	pole_test_1.alpha_ = alpha_1;
+	pole_test_1.eps_ = epsilon_1;
+	
+	pole_test_2.alpha_ = alpha_2;
+	pole_test_2.eps_ = epsilon_2;
+	
+	
+	// Testing part
+	AmiCalc obj;
+	ASSERT_FALSE(obj.pole_equiv(pole_test_1, pole_test_2)) << "Vector epsilon entries are different." ;
+	
+}	
+
+
+//Checks that the function will spit TRUE when poles are identical.
+TEST(pole_equiv, everything_works){
+	// Create two alpha objects
+	AmiCalc::alpha_t alpha_1={0,0,0,1,0};
+	AmiCalc::alpha_t alpha_2={0,0,0,1,0};
+	
+	// Create two epsilon objects
+	AmiCalc::epsilon_t epsilon_1={0,0,0,0,0,1,0};
+	AmiCalc::epsilon_t epsilon_2={0,0,0,0,0,1,0};
+	
+	// Create the pole_struct objects
+	AmiCalc::pole_struct pole_test_1;
+	AmiCalc::pole_struct pole_test_2;
+	
+	// Attach the eps and alpha to the pole_struct
+	pole_test_1.alpha_ = alpha_1;
+	pole_test_1.eps_ = epsilon_1;
+	
+	pole_test_2.alpha_ = alpha_2;
+	pole_test_2.eps_ = epsilon_2;
+	
+	
+	// Testing part
+	AmiCalc obj;
+	ASSERT_TRUE(obj.pole_equiv(pole_test_1, pole_test_2)) << "Alpha and Epsilon vectors are identical for both poles." ;
+	
+}	
+
+
