3e9a1b567364d89f973ec573f131d46e7feff555
James LeBlanc <jpfleblanc@gmail.com>
2020-09-04 09:55:56 -0230

    trying a new wicks_sc - obliterated the old one

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/home/hosam_el/Research_summerr/leblanc_codes/AMI/libami/install   ..
+CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/mnt/c/Users/jpfle/Desktop/working/leblanc_codes/AMI/libami/install   ..
