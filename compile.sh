3042e2723de20cd7452ab41d95466bbfbdd96fbb
James LeBlanc <jpfleblanc@gmail.com>
2022-01-04 14:24:21 -0330

    Cleanup of examples in libami

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/mnt/c/Users/jpfle/Desktop/working/leblanc_codes/AMI/libami:/install -DBUILD_DOC=OFF  ..
+CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/mnt/c/Users/jpfle/Desktop/working/leblanc_codes/AMI/libami/install -DBUILD_DOC=OFF  ..
