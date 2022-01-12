aff836e2c92dcfba61fbb68836f996be973c069d
James LeBlanc <jpfleblanc@gmail.com>
2022-01-12 14:31:34 -0330

    rtd 3

--- doc/conf.py
+++ doc/conf.py
@@ -14,13 +14,39 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# import subprocess, os
+
+# read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+# if read_the_docs_build:
+
+    # subprocess.call('doxygen Doxyfile.in', shell=True)
+
 import subprocess, os
 
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-if read_the_docs_build:
+breathe_projects = {}
 
-    subprocess.call('doxygen Doxyfile.in', shell=True)
+if read_the_docs_build:
+    input_dir = '../libami'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['libami'] = output_dir + '/xml'
+
+
 
 
 # -- Project information -----------------------------------------------------
