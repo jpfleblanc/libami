ac9de5bba9f4d63fee267bc412aa37651fbeee39
James LeBlanc <jpfleblanc@gmail.com>
2022-01-12 22:16:45 -0330

    added flag for testing on release or explicit TEST

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/mnt/c/Users/jpfle/Desktop/working/libami/install -DBUILD_DOC=ON  ..
+CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_INSTALL_PREFIX=/mnt/c/Users/jpfle/Desktop/working/libami/install -DBUILD_DOC=ON  -DTEST=ON ..
