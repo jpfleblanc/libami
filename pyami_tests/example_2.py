c655528139400ae6071eaf2ed778f48265ce9b60
Michael Burke <michaelburke119@gmail.com>
2022-10-11 13:25:44 -0230

    Cleaned up second order example: Seems to work!

--- pyami_tests/example_2.py
+++ pyami_tests/example_2.py
@@ -2,12 +2,13 @@ import pyami
 
 ami = pyami.AmiBase()
 
+# external variables
 energy = pyami.VectorDouble([-4, 0.1, -1])
 freq = [3.14j, 3.14j, 3.14j]
-beta = 1.0
-
+beta = 1.00
 avars = pyami.AmiBase.ami_vars(energy, freq, beta)
 
+# second order self energy setup
 a1 = pyami.VectorInt([1, 0, 0])
 a2 = pyami.VectorInt([0, 1, 0])
 a3 = pyami.VectorInt([-1, 1, 1])
@@ -22,32 +23,19 @@ g3 = pyami.AmiBase.g_struct(e3, a3)
 
 R0 = [g1, g2, g3]
 
+# These are opaque types so that they are mutable in c++ construct function.
+S_array = pyami.S_t()
+P_array = pyami.P_t()
+R_array = pyami.R_t()
 
-# might run into trouble here, vectors in c++ are intialized to be push_backed on to
-# This does not play nice in python :(
-S_array = pyami.S_t()#[[pyami.VectorDouble()]]#[[pyami.VectorDouble()]] # inner most should be doubles
-P_array = pyami.P_t()#[[[]]]#[[[[pyami.AmiBase.pole_struct()]]]] # inner most should be pole_structs
-R_array = pyami.R_t()#[[[]]]#[[[pyami.AmiBase.g_struct()]]] # inner most should be g_structs
-
-E_REG = 0
-N_INT = 2
+E_REG = 0 # numberical regulator
+N_INT = 2 # number of matsubara sums
 test_amiparms = pyami.AmiBase.ami_parms(N_INT, E_REG)
 
 # now construct!
-print("Before the construction: S_t, P_t and R_t are:")
-print(S_array)
-print(P_array)
-print(R_array)
 ami.construct(test_amiparms, R0, R_array, P_array, S_array)
-print("After the construction: S_t, P_t and R_t are:")
-print(S_array)
-print(P_array)
-print(R_array)
 
+# Get integrand for these specific external params
 ans = ami.evaluate(test_amiparms, R_array, P_array, S_array, avars)
 
 print(ans)
-# segfault 11 :(
-
-
-
